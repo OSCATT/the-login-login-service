55413ebf95b668af2945aa4b900e85aed108168f
Oscar Hong <oscarhong0@gmail.com>
2020-02-03 13:46:38 -0500

    ..

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
-
 const PORT = 1234;
+var userCount = 0;
 const app = express();
 
+app.use(express.static('public'))
+
 app.get("/hello", (req, res) => {
-  res.send("Hello world");
+  userCount++;
+    if(userCount%10 == 0){
+    userCount = 0;
+    res.sendFile(__dirname + '/public/tenth.html');
+    }
+    else{
+      res.send("Hello world " + userCount + "th login" + __dirname);
+    }
+});
+
+app.get('/indextest', function(request, response){
+    response.sendFile('absolutePathToYour/htmlPage.html');
 });
 
 app.listen(PORT, () => {
